8d9ee71fea330079fd75d8ae7109e7cc6e289aa2
VISHNU RAJU <rajuvishnu52@gmail.com>
2022-01-23 00:47:14 +0530

    Updated to allow KMS rights

--- scenarios/ec2_ssrf/terraform/iam.tf
+++ scenarios/ec2_ssrf/terraform/iam.tf
@@ -83,10 +83,11 @@ resource "aws_iam_policy" "cg-shepard-policy" {
             "Sid": "shepard",
             "Effect": "Allow",
             "Action": [
-                "lambda:Get*",
-                "lambda:Invoke*",
-                "lambda:List*"
-            ],
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*"
+  ],
             "Resource": "*"
         }
     ]
